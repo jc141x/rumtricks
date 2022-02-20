16ed7babaa611351851f58291ba5e2158ae75f63
goldenboy313 <johncena141@protonmail.com>
2022-02-20 11:37:01 +0000

    Update 'rumtricks.sh'

--- rumtricks.sh
+++ rumtricks.sh
@@ -121,7 +121,7 @@ directx()
     regsvr32 xaudio2_0.dll xaudio2_1.dll xaudio2_2.dll xaudio2_3.dll xaudio2_4.dll xaudio2_5.dll xaudio2_6.dll xaudio2_7.dll
     rm -rf "$PWD"/directx
     installed
-    killall wineserver
+    $WINESERVER -k
 }
 
 vcrun2003()
@@ -252,7 +252,7 @@ mf()
     regsvr32 colorcnv.dll msmpeg2adec.dll msmpeg2vdec.dll
     rm -rf "$PWD"/mf
     installed
-    killall wineserver
+    $WINESERVER -k
 }
 
 vdesktop()
@@ -357,7 +357,7 @@ wmp11()
     regsvr32 dispex.dll jscript.dll scrobj.dll scrrun.dll vbscript.dll wshcon.dll wshext.dll
     rm -rf "$PWD"/wmp11
     installed
-    killall wineserver
+    $WINESERVER -k
 }
 
 mono()
@@ -453,7 +453,7 @@ directshow()
     regsvr32 amstream.dll qasf.dll qcap.dll qdvd.dll qedit.dll quartz.dll
     rm -rf "$PWD"/directshow
     installed
-    killall wineserver
+    $WINESERVER -k
 }
 
 cinepak()
@@ -533,7 +533,7 @@ directplay()
     regsvr32 dplayx.dll dpnet.dll dpnhpast.dll dpnhupnp.dll
     rm -rf "$PWD"/directplay
     installed
-    killall wineserver
+    $WINESERVER -k
 }
 
 dotnet35()
