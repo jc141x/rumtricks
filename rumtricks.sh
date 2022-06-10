430d0157cce28c10023501e1962ee56719d4b48f
mio <mio@noreply.johncena141.eu.org>
2022-06-10 14:15:26 +0000

    Update 'rumtricks.sh'

--- rumtricks.sh
+++ rumtricks.sh
@@ -159,6 +159,16 @@ wine-ge() {
     echo "RMT: Extracting wine-ge" && tar -xvf "wine-ge.tar.lzma" && mv "wine" "$BINDIR/wine"
 }
 
+wine-tkg-nomingw() {
+    [ -x "/bin/wine-tkg-nomingw" ] && echo "RMT: Detected wine-tkg-nomingw installed on system." && exit || echo "RMT: wine-tkg-nomingw not detected on system."
+    [ -x "$BINDIR/wine/bin/wine" ]  && echo "RMT: wine-tkg-nomingw found on relative path." && exit || echo "RMT: wine-tkg-nomingw not found on relative path, downloading."
+    latest_release="$(curl -s https://api.github.com/repos/jc141x/wine-tkg-nomingw/releases | jq '[.[] | select(.tag_name | test(".*[^LoL]$"))][0]')"
+    download_url="$(echo "$latest_release" | awk -F '["]' '/"browser_download_url":/ && /tar.lzma/ {print $4}')"
+    [ ! -f "wine-tkg-nomingw.tar.lzma" ] && echo "RMT: wine-tkg-nomingw.tar.lzma not found, downloading" && curl -L "$download_url" -o "wine-tkg-nomingw.tar.lzma"
+    [ ! -f "wine-tkg-nomingw.tar.lzma" ] && echo "RMT: Download failed, check internet connection" && exit || echo "RMT: wine-tkg-nomingw.tar.lzma downloaded"
+    echo "RMT: Extracting wine-tkg-nomingw" && tar -xvf "wine-tkg-nomingw.tar.lzma" && mv "wine" "$BINDIR/wine"
+}
+
 isolation() {
     $only_cache && return
     status || return
